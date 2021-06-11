7378c44cac8d17f7a8a4bdadcd8cc76d09a151dd
Knappek <andy.knapp.ak@gmail.com>
2021-06-11 16:13:53 +0200

    make script more robust with a sleep

--- gatekeeper-library/apply_gatekeeper_constraints.sh
+++ gatekeeper-library/apply_gatekeeper_constraints.sh
@@ -4,6 +4,9 @@ set -eux
 
 kustomize build library/library/general | kubectl apply -n styra-system -f -
 
+# wait a couple of seconds for all the constraint kinds to become ready
+sleep 4
+
 constraints=$(find library/library/general -name constraint.yaml)
 
 for c in $constraints
